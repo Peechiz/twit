0c3ed2280dac2666da6e61bffde0f9f5f712bbbb
Chris Impicciche <chris.impicciche@customink.com>
2024-03-01 14:00:25 -0600

    user profile stuff

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,5 +1,16 @@
 import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { clerkClient } from "@clerk/nextjs";
+import type { User } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
+
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profilePicture: user.imageUrl,
+  };
+};
 
 export const postRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
@@ -7,6 +18,31 @@ export const postRouter = createTRPCRouter({
       take: 100,
       orderBy: { createdAt: "desc" },
     });
-    return posts;
+
+    const users = (
+      await clerkClient.users.getUserList({
+        limit: 100,
+        userId: posts.map((post) => post.authorId),
+      })
+    ).map(filterUserForClient);
+
+    return posts.map((post) => {
+      const author = users.find((user) => user.id === post.authorId);
+
+      if (!author?.username) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for post not found",
+        });
+      }
+
+      return {
+        post,
+        author: {
+          ...author,
+          username: author.username,
+        },
+      };
+    });
   }),
 });
