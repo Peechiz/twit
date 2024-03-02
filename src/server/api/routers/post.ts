8558c763c2ec97a4102bec2f8c0f9c273210b5ad
Chris Impicciche <chris.impicciche@customink.com>
2024-03-01 18:25:30 -0600

    rate limit

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -7,6 +7,8 @@ import {
 import { clerkClient } from "@clerk/nextjs";
 import type { User } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis"; // see below for cloudflare and fastly adapters
 
 const filterUserForClient = (user: User) => {
   return {
@@ -15,6 +17,19 @@ const filterUserForClient = (user: User) => {
     profilePicture: user.imageUrl,
   };
 };
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(3, "1 m"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */
+  prefix: "@upstash/ratelimit",
+});
 
 export const postRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
@@ -33,12 +48,11 @@ export const postRouter = createTRPCRouter({
     return posts.map((post) => {
       const author = users.find((user) => user.id === post.authorId);
 
-      if (!author?.username) {
+      if (!author?.username)
         throw new TRPCError({
           code: "INTERNAL_SERVER_ERROR",
           message: "Author for post not found",
         });
-      }
 
       return {
         post,
@@ -57,6 +71,13 @@ export const postRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.userId;
+      const { success } = await ratelimit.limit(authorId);
+
+      if (!success)
+        throw new TRPCError({
+          code: "TOO_MANY_REQUESTS",
+        });
+
       const post = await ctx.db.post.create({
         data: {
           content: input.content,
